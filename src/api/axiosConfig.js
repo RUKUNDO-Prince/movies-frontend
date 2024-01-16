4b5d07633e1b466925db901b760514eba8b7167f
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-16 12:13:48 +0200

    some minor changes

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-// axios.interceptors.request.use(function (config) {
-//     config.headers['Access-Control-Allow-Origin'] = '*';
-//     return config;
-//   }, function (error) {
-//     return Promise.reject(error);
-//   });
-
 export default axios.create({
     baseURL:'http://localhost:8080',
     // headers: {"ngrok-skip-browser-warning": "true"}
